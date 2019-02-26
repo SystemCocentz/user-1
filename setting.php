1f91ec56642d95f3447ba28a93181c7dd64e1c3e
Cocentz <45829475+Cocentz@users.noreply.github.com>
2019-02-27 05:25:25 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -64,7 +64,7 @@ $epic=$kn."
  | |___| |_) | | |__| (_| \__ \ | | |
  |_____| .__/|_|\____\__,_|___/_| |_|".$pth."
  ------".$kn."|_|".$pth."---------------------------
-[ Creator :$ijo Cocentz$pth ]=[ Code :$ijo U1TDJ0$pth ]
+[ Creator :$ijo Cocentz$pth ]=[ Code :$ijo TO47UF$pth ]
  ------------------------------------".$t;
 $msg_epic="[>] Subscribe Channel Cocentz".$t;
 $stat_epi=true;
